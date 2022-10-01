7e0efa0f711bc77be35afc92caa0901e70226b0a
egor-shishkarev <egorsiskarev92@gmail.com>
2022-10-02 01:39:21 +0300

    Add a new function that checks for a dot in the filled area.

---
+++ task1/task1.c
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <stdbool.h>
+#include <math.h.>
+
+int verificationFloatScanf() {
+	float readValues = 0;
+	while (true) {
+		int correctlyReadValues = scanf("%f", &readValues);
+		while (getchar() != '\n') {
+		}
+		if (correctlyReadValues == 1) {
+			break;
+		}
+		printf("Ошибка ввода. Введите целочисленное значение!\n");
+	}
+	return readValues;
+}
+
+bool isPointInArea(float x, float y) {
+	x -= 3;
+	y -= 5;
+	return (x) >= 0 && (pow(x, 2) + pow(y, 2) - 4) <= 0;
+}
+
+int main() {
+	setlocale(LC_ALL, ".1251");
+	printf("Ввеидите два значения, для которых хотите проверить логичсекое выражение");
+	printf("\nВведите x-координату => ");
+	float x = verificationFloatScanf();
+	printf("Введите y-координату => ");
+	float y = verificationFloatScanf();
+	printf(isPointInArea(x, y) ? "true" : "flase");
+}
