738c75da5e330e877aff18adbbc23ffbb469a5a0
egor-shishkarev <egorsiskarev92@gmail.com>
2022-10-12 16:16:18 +0300

    Stack

---
+++ steck/steck.c
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node
+{
+    int element;
+    struct Node* next;
+} Node;
+
+int push(Node** head, int element);
+
+int main()
+{
+    Node* head = NULL;
+
+    push(&head, 100);
+    printf("%d", head->element);
+    push(&head, 200);
+    printf("\n%d", head->element);
+    push(&head, 300);
+    printf("\n%d", head->element);
+}
+
+int push(Node** head, int element)
+{
+    Node* temp = malloc(sizeof(Node));
+    if (temp == NULL)
+    {
+        printf("Out of memory\n");
+        return -1;
+    }
+    temp->element = element;
+    temp->next = *head;
+    *head = temp;
+    return 0;
+}
